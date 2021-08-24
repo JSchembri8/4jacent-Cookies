6d05f8728e3664ad1d51f7b13c12d8447140d434
s3721131 <s3721131@student.rmit.edu.au>
2021-08-24 15:59:34 +1000

    Notes/Key functionality added

---
+++ Assets/Scripts/Collected_Script_01.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collected_Script_01 : MonoBehaviour
+{
+    public bool collectedStatus = false;
+
+    void OnMouseOver()
+    {
+        Debug.Log("MOUSEOVER");
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            collectedStatus = true;
+            Debug.Log("Collected" + collectedStatus);
+        }
+    }
+}
---
+++ Assets/Scripts/LockedDoor_Script_01.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor_Script_01 : MonoBehaviour
+{
+    public GameObject[] keyItems;
+    public bool keysCollected = false;
+  
+
+    void Update()
+    {
+        if (keysCollected)
+        {
+            gameObject.SetActive(false);
+        }
+        Debug.Log(keysCollected);
+        keysCollected = AllKeysCollected();
+    }
+
+    private bool AllKeysCollected()
+    {
+        for (int g = 0; g < keyItems.Length; g++)
+        {
+            if (keyItems[g].GetComponent<Collected_Script_01>().collectedStatus == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
